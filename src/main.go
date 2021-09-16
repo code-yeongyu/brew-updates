00c7f7c8d13edf0136a0fd066cca3150955634d9
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2021-09-16 23:56:29 +0900

    Update main to run `brew thing` when it's required

--- src/main.go
+++ src/main.go
@@ -1,21 +1,65 @@
 package main
 
 import (
+	"brew-updates/utils"
 	"brew-updates/utils/brew"
+	"brew-updates/utils/history"
 	"fmt"
 )
 
-func main() {
-	brewManager := brew.BrewManager{}
-
-	if !brewManager.IsInstalled() {
-		fmt.Println("Brew is not installed.")
-		return
+func checkExecutable(brewManager brew.BrewManager) {
+	if !utils.IsMacOS() {
+		panic("Only macOS is supported.")
 	}
+	if !brewManager.IsInstalled() {
+		panic("Brew is not installed.")
+	}
+}
 
+func getRecordedBrewUpgradeCount() int {
+	countFileManager := history.GetCountFileManager()
+	recordedBrewUpgradeCount, _ := countFileManager.GetCount()
+	return recordedBrewUpgradeCount
+}
+
+func getCurrentBrewUpgradeCount() int {
+	historyFileManager := history.GetHistoryFileManager()
+	historyFileManager.UpdateHistory()
+	commandHistory, _ := historyFileManager.GetHistory()
+	return history.CountBrewUpgrade(commandHistory)
+}
+
+func isBrewUpgradeCountHasDiffer(currentBrewUpgradeCount int) bool {
+	recordedBrewUpgradeCount := getRecordedBrewUpgradeCount()
+	return recordedBrewUpgradeCount != currentBrewUpgradeCount
+}
+
+func getUpgradablePackagesCount(brewManager brew.BrewManager) int {
 	brewManager.UpdateInfo()
 	upgradablePackages := brewManager.GetUpgradablePackages()
-	upgradablePackagesCnt := len(upgradablePackages)
+	return len(upgradablePackages)
+}
+
+func main() {
+	brewManager := brew.BrewManager{}
+	checkExecutable(brewManager)
+
+	currentBrewUpgradeCount := getCurrentBrewUpgradeCount()
+	updatablePackageCacheManager := brew.GetUpdatablePackagesCacheManager()
+
+	if isBrewUpgradeCountHasDiffer(currentBrewUpgradeCount) {
+		historyCountManager := history.GetCountFileManager()
+		if err := historyCountManager.SaveCount(currentBrewUpgradeCount); err != nil {
+			panic(err)
+		}
+
+		upgradablePackagesCount := getUpgradablePackagesCount(brewManager)
+		if err := updatablePackageCacheManager.SaveCount(upgradablePackagesCount); err != nil {
+			panic(err)
+		}
+	}
+
+	upgradablePackagesCnt, _ := updatablePackageCacheManager.GetCount()
 
 	fmt.Printf("%d updates can be applied immediately.", upgradablePackagesCnt)
 	if upgradablePackagesCnt > 10 {
